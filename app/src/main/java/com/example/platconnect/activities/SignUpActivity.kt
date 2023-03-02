0d36ced4dcc67bc84c710a894beb343b275a5c41
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-03-02 13:14:45 +0100

    signin started

---
+++ app/src/main/java/com/example/platconnect/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.platconnect.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.platconnect.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
