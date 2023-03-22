f98465a9443b703d3e80a76e83cecc963075540c
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-03-22 16:06:35 +0100

    signup uptades

--- app/src/main/java/com/example/ducktalk/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/ducktalk/activities/SignUpActivity.kt
@@ -1,10 +1,19 @@
 package com.example.ducktalk.activities
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import android.util.Patterns
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ducktalk.databinding.ActivitySignUpBinding
+import java.io.ByteArrayOutputStream
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -31,6 +40,41 @@ class SignUpActivity : AppCompatActivity() {
     private fun showToast(message: String) {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
+
+    private fun signUp() {
+        // Add your sign-up code here
+    }
+
+    private fun encodeImage(bitmap: Bitmap): String {
+        val previewWidth = 150
+        val previewHeight = bitmap.height * previewWidth / bitmap.width
+        val previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
+        val bytes = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(bytes, Base64.DEFAULT)
+    }
+
+    private val pickImage = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val imageData: Intent? = result.data
+            imageData?.data?.let { imageUri ->
+                try {
+                    val inputStream: InputStream? = contentResolver.openInputStream(imageUri)
+                    val bitmap: Bitmap? = BitmapFactory.decodeStream(inputStream)
+                    binding.imageProfile.setImageBitmap(bitmap)
+                    binding.textAddImage.visibility = View.GONE
+                    encodedImage = bitmap?.let { encodeImage(it) }
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+
 
     private fun isValidSignUpDetails(): Boolean {
         return when {
@@ -66,7 +110,13 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun signUp() {
-        // Add your sign-up code here
+    private fun loading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.buttonSignUp.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.buttonSignUp.visibility = View.VISIBLE
+        }
     }
 }
---
+++ app/src/main/java/com/example/ducktalk/activities/utilities/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.ducktalk.activities.utilities
+ object Constants {
+    const val KEY_COLLECTION_USERS = "users"
+    const val KEY_NAME = "name"
+    const val KEY_EMAIL = "email"
+    const val KEY_PASSWORD = "password"
+    const val KEY_PREFERENCE_NAME = "DuckTalkPreference"
+    const val KEY_IS_SIGNED_IN = "isSignedIn"
+    const val KEY_USER_ID = "userid"
+    const val KEY_IMAGE = "image"
+}
