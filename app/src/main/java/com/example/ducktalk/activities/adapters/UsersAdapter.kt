a114c6cebbc0560a20d9d7fcf0a60063b7e07fbc
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-03-23 13:07:40 +0100

    logout and chatapp

--- app/src/main/java/com/example/ducktalk/activities/adapters/UsersAdapter.kt
+++ app/src/main/java/com/example/ducktalk/activities/adapters/UsersAdapter.kt
@@ -1,4 +1,41 @@
 package com.example.ducktalk.activities.adapters
 
-class UsersAdapter {
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ducktalk.activities.models.User
+import com.example.ducktalk.databinding.ItemContainerUserBinding
+
+class UsersAdapter(private val users: List<User>) : RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemContainerUserBinding.inflate(inflater, parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.setUserData(users[position])
+    }
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    inner class UserViewHolder(private val binding: ItemContainerUserBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun setUserData(user: User) {
+            binding.textName.text = user.name
+            binding.textEmail.text = user.email
+            binding.imageProfile.setImageBitmap(getUserImage(user.image))
+        }
+    }
+
+    private fun getUserImage(encodedImage: String): Bitmap {
+        val bytes = Base64.decode(encodedImage, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
 }
