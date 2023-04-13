efa05745988af009bae08b27c669315a8fa354cf
r4ul1 <55692502+r4ul1@users.noreply.github.com>
2023-04-13 11:27:00 +0200

    main_activity_signout_user_list

--- app/src/main/java/com/example/ducktalk/activities/MainActivity.kt
+++ app/src/main/java/com/example/ducktalk/activities/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.example.ducktalk.activities
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.widget.Toast
+import com.example.ducktalk.activities.databinding.ActivityMainBinding
+import com.example.ducktalk.activities.utilities.Constants
+import com.example.ducktalk.activities.utilities.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var preferenceManager: PreferenceManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        preferenceManager = PreferenceManager(applicationContext)
+        loadUserDetails()
+        /**getToken()**/
+        setListeners()
+    }
+
+    private fun setListeners() {
+        /**binding.imageSignOut.setOnClickListener { signOut() }**/
+        binding.fabNewChat.setOnClickListener { startActivity(Intent(applicationContext, UsersActivity::class.java)) }
+    }
+
+    private fun loadUserDetails() {
+        binding.textName.text = preferenceManager.getString(Constants.KEY_NAME)
+        val bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT)
+        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+        binding.imageProfile.setImageBitmap(bitmap)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    /**private fun getToken() {
+        FirebaseMessaging.getInstance().token.addOnSuccessListener(this@MainActivity, this::updateToken)
+    }
+
+    private fun updateToken(token: String) {
+        val database = FirebaseFirestore.getInstance()
+        val documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(preferenceManager.getString(Constants.KEY_USER_ID))
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+            .addOnFailureListener { e -> showToast("Unable to update token") }
+    }
+
+    private fun signOut() {
+        showToast("Signing out...")
+        val database = FirebaseFirestore.getInstance()
+        val documentReference = database.collection(Constants.KEY_COLLECTION_USERS).document(preferenceManager.getString(Constants.KEY_USER_ID))
+        val updates = HashMap<String, Any>()
+        updates[Constants.KEY_FCM_TOKEN] = FieldValue.delete()
+        documentReference.update(updates)
+            .addOnSuccessListener {
+                preferenceManager.clear()
+                startActivity(Intent(applicationContext, SignInActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { e -> showToast("Unable to sign out") }**/
     }
 }
+
